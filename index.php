7340e5230d08397048030817ec9a5027e8924fcc
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 01:31:54 +0200

    am adaugat un bloc informativ despre produsele ce urmeaza sa fie plasate pe acest sit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+	<meta http-eqiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Magazin online</title>
+</head>
+<body>
+<?php require "blok/header.php" ?>
+<div class="container mt-5">
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <p class="lead"> La noi poți găsi o selecție largă de produse - buchete dulci, compoziții florale şi buchețele mici, deasemenea suntem gata să realizăm la comandă orice creație doriți dvs. Produsele noastre vor fi cadoul perfect ce va readuce lumină și culori vii în zilele de rutină și vor crea o dispoziție de sărbătoare. </p>
+</div>
+</div>
+<?php require "blok/footer.php" ?>
+</body>
+</html>
