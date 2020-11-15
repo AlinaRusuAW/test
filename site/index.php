2deb18198855d3934ac9330396ec2817c63483dd
AlinaRusuAW <galaicuc213@gmail.com>
2020-11-16 01:28:06 +0200

    Buna acesta e primul meu proiect in git

---
+++ site/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=divice-width, initial-scale=1.0">
+	<meta http-eqiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Magazin online</title>
+</head>
+<body>
+<?php require "blok/header.php" ?>
+<?php require "blok/footer.php" ?>
+</body>
+</html>
